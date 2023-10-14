4878b757677b017c7fc65bd516e8e6db2d5b8c6e
qqKazakov <leap74rus@gmail.com>
2023-10-14 13:38:11 +0300

    add: first models

--- www/index.php
+++ www/index.php
@@ -1,1 +1,8 @@
 <?php
+
+require __DIR__ . '/../src/MyProject/Models/Users/User.php';
+require __DIR__ . '/../src/MyProject/Models/Articles/Article.php';
+
+$author = new User('Иван');
+$article = new Article('Заголовок', 'Текст', $author);
+var_dump($article);
