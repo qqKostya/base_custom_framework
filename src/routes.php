7b3a042c6a3f7f669c7357cf2363a4e97bf895a4
qqKazakov <leap74rus@gmail.com>
2023-10-17 08:59:27 +0300

    add: new router and new action

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,6 @@
 
 return [
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
+    '~^bye/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBye'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
 ];
