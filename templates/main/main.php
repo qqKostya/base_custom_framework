8ac88dbe5b51c41ffe5ddb5004588416cd8e4804
qqKazakov <leap74rus@gmail.com>
2023-10-17 09:12:54 +0300

    add: view and template main action

---
+++ templates/main/main.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Мой блог</title>
+    <link rel="stylesheet" href="/styles.css">
+</head>
+<body>
+
+<table class="layout">
+    <tr>
+        <td colspan="2" class="header">
+            Мой блог
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <?php foreach ($articles as $article): ?>
+                <h2><?= $article['name'] ?></h2>
+                <p><?= $article['text'] ?></p>
+                <hr>
+            <?php endforeach; ?>
+        </td>
+
+        <td width="300px" class="sidebar">
+            <div class="sidebarHeader">Меню</div>
+            <ul>
+                <li><a href="/">Главная страница</a></li>
+                <li><a href="/about-me">Обо мне</a></li>
+            </ul>
+        </td>
+    </tr>
+    <tr>
+        <td class="footer" colspan="2">Все права защищены (c) Мой блог</td>
+    </tr>
+</table>
+
+</body>
+</html>
