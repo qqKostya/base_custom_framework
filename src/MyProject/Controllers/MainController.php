8e45c40b343185e3ca244292e8b00b7f09a88c78
qqKazakov <leap74rus@gmail.com>
2023-10-17 10:14:01 +0300

    test: controller connect to db

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -2,23 +2,31 @@
 
 namespace MyProject\Controllers;
 
+use MyProject\Services\Db;
 use MyProject\View\View;
 class MainController
 {
+    /** @var View */
     private $view;
+
+    /** @var Db */
+    private $db;
 
     public function __construct()
     {
         $this->view = new View(__DIR__ . '/../../../templates');
+        $this->db = new Db();
     }
     public function main()
     {
-        $articles = [
-            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
-            ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
-        ];
-        $title = '';
-        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
+        $articles = $this->db->query('SELECT * FROM `articles`;');
+        var_dump($articles);
+//        $articles = [
+//            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
+//            ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
+//        ];
+//        $title = '';
+//        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
     }
 
     public function sayHello(string $name)
