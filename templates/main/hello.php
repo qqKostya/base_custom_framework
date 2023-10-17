f5d088adaf154ca14d3865b85162a3ff4346c0da
qqKazakov <leap74rus@gmail.com>
2023-10-17 09:38:48 +0300

    add: template header and footer

--- templates/main/hello.php
+++ templates/main/hello.php
@@ -1,35 +1,3 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Мой блог</title>
-    <link rel="stylesheet" href="/styles.css">
-</head>
-<body>
-
-<table class="layout">
-    <tr>
-        <td colspan="2" class="header">
-            Мой блог
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Привет, <?= $name ?>!!!
-        </td>
-
-        <td width="300px" class="sidebar">
-            <div class="sidebarHeader">Меню</div>
-            <ul>
-                <li><a href="/">Главная страница</a></li>
-                <li><a href="/about-me">Обо мне</a></li>
-            </ul>
-        </td>
-    </tr>
-    <tr>
-        <td class="footer" colspan="2">Все права защищены (c) Мой блог</td>
-    </tr>
-</table>
-
-</body>
-</html>
+<?php include __DIR__ . '/../header.php'; ?>
+    Привет, <?= $name ?>!!!
+<?php include __DIR__ . '/../footer.php'; ?>
--- templates/main/main.php
+++ templates/main/main.php
@@ -1,39 +1,7 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Мой блог</title>
-    <link rel="stylesheet" href="/styles.css">
-</head>
-<body>
-
-<table class="layout">
-    <tr>
-        <td colspan="2" class="header">
-            Мой блог
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <?php foreach ($articles as $article): ?>
-                <h2><?= $article['name'] ?></h2>
-                <p><?= $article['text'] ?></p>
-                <hr>
-            <?php endforeach; ?>
-        </td>
-
-        <td width="300px" class="sidebar">
-            <div class="sidebarHeader">Меню</div>
-            <ul>
-                <li><a href="/">Главная страница</a></li>
-                <li><a href="/about-me">Обо мне</a></li>
-            </ul>
-        </td>
-    </tr>
-    <tr>
-        <td class="footer" colspan="2">Все права защищены (c) Мой блог</td>
-    </tr>
-</table>
-
-</body>
-</html>
+<?php include __DIR__ . '/../header.php'; ?>
+<?php foreach ($articles as $article): ?>
+    <h2><?= $article['name'] ?></h2>
+    <p><?= $article['text'] ?></p>
+    <hr>
+<?php endforeach; ?>
+<?php include __DIR__ . '/../footer.php'; ?>
