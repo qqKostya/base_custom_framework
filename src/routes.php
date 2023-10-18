c524ae200cc60e651ddf653770e7ecdfb2cb745e
qqKazakov <leap74rus@gmail.com>
2023-10-18 08:42:30 +0300

    add: template errors and router, controller, view for  articles

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
     '~^bye/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBye'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
