3e430029349d1b9d4853af56a13ad354748d80ef
qqKazakov <leap74rus@gmail.com>
2023-10-20 09:50:27 +0300

    add: new method in actionrecord and refactoring ArticlesController

--- src/MyProject/Controllers/ArticlesController.php
+++ src/MyProject/Controllers/ArticlesController.php
@@ -2,10 +2,7 @@
 
 namespace MyProject\Controllers;
 
-namespace MyProject\Controllers;
-
 use MyProject\Models\Articles\Article;
-use MyProject\Services\Db;
 use MyProject\View\View;
 
 class ArticlesController
@@ -13,40 +10,20 @@ class ArticlesController
     /** @var View */
     private $view;
 
-    /** @var Db */
-    private $db;
-
     public function __construct()
     {
         $this->view = new View(__DIR__ . '/../../../templates');
-        $this->db = new Db();
     }
 
     public function view(int $articleId)
     {
-        $result = $this->db->query(
-            'SELECT * FROM `articles` WHERE id = :id;',
-            [':id' => $articleId],
-            Article::class
-        );
-        var_dump($result);
+        $article = Article::getById($articleId);
 
-
-        if ($result === []) {
+        if ($article === null) {
             $this->view->renderHtml('errors/404.php', [], 404);
             return;
         }
 
-        $result_user = $this->db->query(
-            'SELECT `nickname` FROM `users` WHERE id = :id',
-            [':id' => $result->id]
-        );
-        var_dump($result_user);
-
-        if ($result_user === []) {
-            $result_user[0]['nickname'] = 'не известно';
-        }
-
-        $this->view->renderHtml('articles/view.php', ['article' => $result[0], 'nickname' => $result_user[0]['nickname']]);
+        $this->view->renderHtml('articles/view.php', ['article' => $article]);
     }
 }
--- src/MyProject/Models/ActiveRecordEntity.php
+++ src/MyProject/Models/ActiveRecordEntity.php
@@ -38,4 +38,19 @@ abstract class ActiveRecordEntity
     }
 
     abstract protected static function getTableName(): string;
+
+    /**
+     * @param int $id
+     * @return static|null
+     */
+    public static function getById(int $id): ?self
+    {
+        $db = new Db();
+        $entities = $db->query(
+            'SELECT * FROM `' . static::getTableName() . '` WHERE id=:id;',
+            [':id' => $id],
+            static::class
+        );
+        return $entities ? $entities[0] : null;
+    }
 }
