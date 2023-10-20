75b8a248e0d6d31a445ea8869613145e77b170e1
qqKazakov <leap74rus@gmail.com>
2023-10-20 09:52:07 +0300

    fix: view template articles

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -1,5 +1,4 @@
 <?php include __DIR__ . '/../header.php'; ?>
     <h1><?= $article->getName() ?></h1>
     <p><?= $article->getText() ?></p>
-<!--    <p>Имя автора: --><?php //= $nickname ?><!--</p>-->
 <?php include __DIR__ . '/../footer.php'; ?>
