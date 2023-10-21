ac25bbc24f0a7495dcaa3a3fc2c2decf51d1cf1e
qqKazakov <leap74rus@gmail.com>
2023-10-21 09:46:22 +0300

    add: exception connect to bd

--- src/settings.php
+++ src/settings.php
@@ -2,7 +2,7 @@
 return [
     'db' => [
         'host' => 'localhost',
-        'dbname' => 'my_project',
+        'dbname' => 'my_project2',
         'user' => 'root',
         'password' => 'root',
     ]
